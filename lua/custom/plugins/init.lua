5518dc4a1a6ee236126d01bb3e7c451500e518b5
Hovinarri <miki.pelkonen@gmail.com>
2025-09-05 18:39:20 +0300

    Change color scheme and add custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,87 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'williamboman/mason.nvim',
+    config = true,
+  },
+  {
+
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require('mason-lspconfig').setup {
+        ensure_installed = {
+          'pyright',
+          'rust_analyzer',
+          'omnisharp',
+          'tsserver',
+          'lua ls',
+        },
+      }
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      local lspconfig = require 'lspconfig'
+
+      local servers = {
+        pyright = {},
+        rust_analyzer = {},
+        omnisharp = {},
+        jdtls = {},
+        tsserver = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = { globals = { 'vim' } },
+            },
+          },
+        },
+      }
+
+      for server, config in pairs(servers) do
+        lspconfig[server].setup(config)
+      end
+    end,
+  },
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        python = { 'black' },
+        rust = { 'rustfmt' },
+        csharp = { 'csharpier' },
+        java = { 'google-java-format' },
+        typescript = { 'prettier' },
+        javascript = { 'prettier' },
+        lua = { 'stylua' },
+      },
+    },
+  },
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = 'LazyGit',
+    keys = {
+      { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'Lazygit' },
+    },
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = function()
+      require('toggleterm').setup {
+        direction = 'float',
+      }
+      vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm<cr>', { desc = 'Terminal' })
+    end,
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
+    config = function()
+      require('telescope').load_extension 'fzf'
+    end,
+  },
+}
